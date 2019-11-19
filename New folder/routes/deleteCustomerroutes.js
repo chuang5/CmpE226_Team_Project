c1a294c8301beeec3db33a0795412763c59b5f59
chuang5 <chen-feng.huang@sjsu.edu>
2019-11-18 17:09:34 -0800

    copy from mengchao_Li's repo

---
+++ New folder/routes/deleteCustomerroutes.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '6112049qaZ',
+//     database: 'cloudprint'
+// });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '98765432',
+    database: '226team'
+});
+
+exports.deleteCustomer = function(req,res){
+    // console.log("req",req.body);
+    const today = new Date();
+    const account_no = req.body.account_no;
+
+    connection.query('DELETE FROM customers WHERE account_no = ?',account_no, function (error, results, fields) {
+        if (error) {
+            console.log("error occurred",error);
+            res.send({
+                "code":400,
+                "failed":"error occurred"
+            })
+        }else{
+            console.log('The solution is: ', results);
+            res.send({
+                "code":200,
+                "success":"customers deleted successfully"
+            });
+        }
+    });
+}
