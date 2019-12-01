c294fc6048a33f11b2776ec4ef9b79f2b0b5e7c3
Zixin Li <kathy31629@gmail.com>
2019-11-30 17:05:39 -0800

    Update add_foreign_keys.sql

--- Server_Banking/sql/add_foreign_keys.sql
+++ Server_Banking/sql/add_foreign_keys.sql
@@ -1,11 +1,8 @@
 ALTER TABLE checking
-ADD CONSTRAINT FK_checkingID
-FOREIGN KEY (customer) REFERENCES customers(customer_id);
+ADD FOREIGN KEY (customer) REFERENCES customers(customer_id);
 
 ALTER TABLE saving
-ADD CONSTRAINT FK_savingID
-FOREIGN KEY (customer) REFERENCES customers(customer_id);
+ADD FOREIGN KEY (customer) REFERENCES customers(customer_id);
 
 ALTER TABLE credit_card
-ADD CONSTRAINT FK_ccID
-FOREIGN KEY (customer) REFERENCES customers(customer_id);
+ADD FOREIGN KEY (customer) REFERENCES customers(customer_id);
